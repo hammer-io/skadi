945ec052b25feb274e4df0f595e453446df1d371
Erica Clark <enclark@iastate.edu>
2018-02-06 20:48:58 -0600

    Adds async/await and more documentation to the heartbeat library

--- src/heartbeat/heartbeat.js
+++ src/heartbeat/heartbeat.js
@@ -8,16 +8,39 @@ let id = '';
 let serverUrl = '';
 let interval = TEN_MINUTES;
 
-function heartbeat() {
-  axios.post(`${serverUrl}${HEARTBEAT_ENDPOINT}`, { id }, { headers: { 'Content-Type': 'application/json' } })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+/**
+ * A function that posts a heartbeat containing only its id to the server. If an error occurs when
+ * trying to connect to the server, the heartbeat will continue to repeat, but the error will be
+ * logged.
+ *
+ * @returns {Promise<void>} - The promise
+ */
+async function heartbeat() {
+  try {
+    const response = await axios.post(
+      `${serverUrl}${HEARTBEAT_ENDPOINT}`,
+      { id },
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+    console.log(response.data);
+    if (response.data !== 'success') {
+      console.log('An error probably occurred when trying to' +
+      ` post a heartbeat to ${serverUrl}${HEARTBEAT_ENDPOINT}.  Returned Data: ${response.data}`);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
+/**
+ * Sets the configurable features of the heartbeat.  The options object must contain an id for
+ * the project and the url of the Koma server.  Other configurable options are the interval at
+ * which the heartbeat repeats, which must be at least one minute and is specified in milliseconds.
+ * If the interval is not set, it defaults to 10 minutes. Once the options have been set, the
+ * heartbeat function is run, then set to run repeatedly for the specified interval.
+ *
+ * @param options - The options object
+ */
 export default function setHeartbeat(options) {
   if (options.id) {
     ({ id } = options);
